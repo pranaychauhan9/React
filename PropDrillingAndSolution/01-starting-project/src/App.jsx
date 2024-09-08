ef3eb2e1d40647aaabb59bc46333a1c23ec828e9
Pranay Chauhan <pranaychauhan54@gmail.com>
2024-09-09 03:25:46 +0530

    Learn What is Propdrilling and how can we avoid the propdrilling

---
+++ PropDrillingAndSolution/01-starting-project/src/App.jsx
@@ -0,0 +1,23 @@
+import Product from './components/Product.jsx';
+import CartContextProvider, { CartContext } from './store/shopping-cart-context.jsx';
+import Header from './components/Header.jsx';
+import Shop from './components/Shop.jsx';
+import { DUMMY_PRODUCTS } from './dummy-products.js';
+
+function App() {
+  
+  return (
+    <CartContextProvider>
+      <Header />
+      <Shop  > 
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} />
+          </li>
+        ))}</Shop>
+
+    </CartContextProvider>
+  );
+}
+
+export default App;
